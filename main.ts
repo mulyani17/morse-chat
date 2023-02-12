274c03685643571b8e8d47f37879393f4cbbd9b1
mulyani17 <125170881+mulyani17@users.noreply.github.com>
2023-02-12 11:25:21 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(0)
+})

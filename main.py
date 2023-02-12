2c5a4866eb88ef67fb8ee06a2a0eabcf7e370fd0
mulyani17 <125170881+mulyani17@users.noreply.github.com>
2023-02-12 13:18:08 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def on_received_number(receivedNumber):
                         . . . . .
         """)
         music.play_tone(262, music.beat(BeatFraction.QUARTER))
-    else:
+    elif receivedNumber == 1:
         basic.show_leds("""
             . . . . .
                         . . . . .
@@ -17,6 +17,15 @@ def on_received_number(receivedNumber):
                         . . . . .
         """)
         music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    else:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+        basic.pause(500)
     basic.clear_screen()
 radio.on_received_number(on_received_number)
 
@@ -26,10 +35,37 @@ input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
 
 def on_button_pressed_a():
     radio.send_number(0)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+    """)
+    music.play_tone(262, music.beat(BeatFraction.QUARTER))
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    radio.send_number(2)
+    basic.show_leds("""
+        # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+    """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     radio.send_number(1)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+    """)
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
--- main.ts
+++ main.ts
@@ -1,46 +1,70 @@
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     if (receivedNumber == 0) {
         basic.showLeds(`
             . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        `)
         music.playTone(262, music.beat(BeatFraction.Quarter))
     } else if (receivedNumber == 1) {
         basic.showLeds(`
             . . . . .
-            . . . . .
-            . # # # .
-            . . . . .
-            . . . . .
-            `)
+                        . . . . .
+                        . # # # .
+                        . . . . .
+                        . . . . .
+        `)
         music.playTone(262, music.beat(BeatFraction.Whole))
     } else {
         basic.showLeds(`
             # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        `)
         basic.pause(500)
     }
+    
     basic.clearScreen()
 })
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+input.onLogoEvent(TouchButtonEvent.LongPressed, function on_logo_long_pressed() {
     radio.sendNumber(1)
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     radio.sendNumber(0)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+    `)
+    music.playTone(262, music.beat(BeatFraction.Quarter))
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     radio.sendNumber(2)
+    basic.showLeds(`
+        # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+    `)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     radio.sendNumber(1)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+    `)
+    music.playTone(262, music.beat(BeatFraction.Whole))
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     radio.sendNumber(0)
 })
